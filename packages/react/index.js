2a88f9da8bf48cdad6a6d4b8fe14ff4b9b1f080a
杨勇 <1994101600@qq.com>
2021-12-01 10:36:59 +0800

    format: react

--- packages/react/index.js
+++ packages/react/index.js
@@ -2,10 +2,10 @@ module.exports = {
   extends: ['plugin:react/recommended', '@imyangyong/eslint-config-ts'],
   settings: {
     react: {
-      version: '17.0'
-    }
+      version: '17.0',
+    },
   },
   rules: {
-    'jsx-quotes': ['error', 'prefer-double']
-  }
+    'jsx-quotes': ['error', 'prefer-double'],
+  },
 }
